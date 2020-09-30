7181219c4a40e08cc2556fc1fae8dade5c88a393
Sean Carver <seancarverphd@gmail.com>
2020-09-30 13:10:21 -0400

    Apparently works, seems to converge

--- tele/findjam.py
+++ tele/findjam.py
@@ -18,7 +18,7 @@ class Jams():
         self.comm = self.teleport(self.ngrid)
 
     def teleport_jammer(self):
-        self.jammer = self.teleport(self.grid) 
+        self.jammer = self.teleport(self.ngrid) 
 
     def teleport(self, ngrid):
         return (np.random.choice(ngrid), np.random.choice(ngrid))
@@ -27,7 +27,7 @@ class Jams():
         return np.log(scipy.special.expit(2*self.slope*x))
 
     def loglikelihood(self):
-        return self.logsig(self.sig(np.sqrt((self.jx - self.comm[0])**2 + (self.jy - self.comm[1])**2)))
+        return self.logsig(np.sqrt((self.jx - self.comm[0])**2 + (self.jy - self.comm[1])**2))
 
     def run(self, steps=3):
         for _ in range(steps):
