0f6fc10d9240cd29dfb140beff0b40b74a92e0b6
Sean Carver <seancarverphd@gmail.com>
2020-09-30 12:29:38 -0400

    findjams.py ready for testing, render() not yet implemented

--- tele/findjam.py
+++ tele/findjam.py
@@ -9,6 +9,8 @@ class Jams():
         self.asset = (ngrid-1,ngrid-1)
         self.teleport_comm()
         self.teleport_jammer()
+        self.jx = np.arange(5).reshape(1,5)*np.ones((5,1))
+        self.jy = np.arange(5).reshape(5,1)*np.ones((1,5))
         # All distributions are represented as logs for stability
         self.logPjammer_prior = np.ones((ngrid, ngrid))*(-2.)*np.log(ngrid) # logProb(jammer@loc); init to uniform
 
@@ -21,11 +23,18 @@ class Jams():
     def teleport(self, ngrid):
         return (np.random.choice(ngrid), np.random.choice(ngrid))
 
+    def logsig(self, x):
+        return np.log(scipy.special.expit(2*self.slope*x))
+
     def loglikelihood(self):
-        pass
+        return self.logsig(self.sig(np.sqrt((self.jx - self.comm[0])**2 + (self.jy - self.comm[1])**2)))
 
-    def run(self, steps):
+    def run(self, steps=3):
         for _ in range(steps):
-        logPjammer_unnormalized = self.loglikelihood() + self.logPjammer_prior
-        self.logPjammer_prior = scipy.special.log_softmax(logPjammer_unnormalized)  # Prior updated to Posterior
+            logPjammer_unnormalized = self.loglikelihood() + self.logPjammer_prior
+            self.logPjammer_prior = scipy.special.log_softmax(logPjammer_unnormalized)  # Prior updated to Posterior
+            self.render()
+            self.teleport_comm()
 
+    def render(self):
+        pass
