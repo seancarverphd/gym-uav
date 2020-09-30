9ac539e6c30debf6bbd7aad7abc7bf9e454217b9
Sean Carver <seancarverphd@gmail.com>
2020-09-30 18:23:24 -0400

    comms and jammers now a list of possibly many---plots all but just computes on first one

--- tele/findjam.py
+++ tele/findjam.py
@@ -3,9 +3,12 @@ import scipy.special
 import matplotlib.pylab as plt
 
 class Jams():
-    def __init__(self, ngrid=5, slope=1.):
+    def __init__(self, ngrid=5, ncomms=1, njams=1, slope=1.):
         self.step = 0
+        self.smallest = 1e-323
         self.ngrid = ngrid
+        self.ncomms = ncomms
+        self.njams = njams
         self.slope = slope
         self.hq = (0,0)
         self.asset = (ngrid-1,ngrid-1)
@@ -13,16 +16,20 @@ class Jams():
         self.jammer = None
         self.teleport_comm()
         self.teleport_jammer()
-        self.jx = np.arange(5).reshape(1,5)*np.ones((5,1))
-        self.jy = np.arange(5).reshape(5,1)*np.ones((1,5))
+        self.jx = np.arange(ngrid).reshape(1,ngrid)*np.ones((ngrid,1))
+        self.jy = np.arange(ngrid).reshape(ngrid,1)*np.ones((1,ngrid))
         # All distributions are represented as logs for stability
         self.logPjammer_prior = np.ones((ngrid, ngrid))*(-2.)*np.log(ngrid) # logProb(jammer@loc); init to uniform
 
     def teleport_comm(self):
-        self.comm = self.teleport(self.ngrid)
+        self.comm = []
+        for _ in range(self.ncomms):
+            self.comm.append(self.teleport(self.ngrid))
 
     def teleport_jammer(self):
-        self.jammer = self.teleport(self.ngrid) 
+        self.jammer = []
+        for _ in range(self.njams):
+            self.jammer.append(self.teleport(self.ngrid))
 
     def teleport(self, ngrid):
         return (np.random.choice(ngrid), np.random.choice(ngrid))
@@ -37,11 +44,13 @@ class Jams():
         else:
             jx = jam[0]   # single float, one location
             jy = jam[1]   # single float, one location
-        return self.sig(np.sqrt((jx - self.comm[0])**2 + (jy - self.comm[1])**2) -
-                        np.sqrt((target[0] - self.comm[0])**2 + (target[1] - self.comm[1])**2))
+        kc = 0  # need to make this a loop
+        return self.sig(np.sqrt((jx - self.comm[kc][0])**2 + (jy - self.comm[kc][1])**2) -
+                        np.sqrt((target[0] - self.comm[kc][0])**2 + (target[1] - self.comm[kc][1])**2))
 
     def contact(self, target):
-        jam = self.jammer
+        kj = 0  # need to make this a loop
+        jam = self.jammer[kj]
         p = self.likelihood(target, jam)
         return np.random.choice([True, False],p=(p, 1.-p)) 
 
@@ -52,11 +61,13 @@ class Jams():
 
     def run(self, steps=1):
         for _ in range(steps):
+            # need to loop these over comms
             self.asset_contacted = self.contact(self.asset)       # True/False at veridical jammer location
             self.hq_contacted = self.contact(self.hq)          # True/False at veridical jammer location
+            # also need to contact comms <--> comms
             self.p_obs_asset = self.likelihood_obs(self.asset, self.asset_contacted)
             self.p_obs_hq = self.likelihood_obs(self.hq, self.hq_contacted)
-            self.logPjammer_unnormalized = np.log(self.p_obs_asset) + np.log(self.p_obs_hq) + self.logPjammer_prior
+            self.logPjammer_unnormalized = np.log(self.p_obs_asset + self.smallest) + np.log(self.p_obs_hq + self.smallest) + self.logPjammer_prior
             self.logPjammer_prior = scipy.special.log_softmax(self.logPjammer_unnormalized)  # Prior updated to Posterior
             self.teleport_comm()
             self.step += 1
@@ -64,9 +75,10 @@ class Jams():
     def render(self):
         l = plt.imshow(self.logPjammer_prior, cmap='hot', interpolation='nearest')
         plt.text(self.asset[0], self.asset[1],"Asset")
-        plt.text(self.jammer[0], self.jammer[1],"Jammer")
         plt.text(self.hq[0], self.hq[1],"Headquarters")
-        plt.text(self.comm[0], self.comm[1],"Comm")
+        for kc in range(self.ncomms):
+            plt.text(self.comm[kc][0], self.comm[kc][1],"Comm")
+        for kj in range(self.njams):
+            plt.text(self.jammer[kj][0], self.jammer[kj][1],"Jammer")
         plt.title("Steps = " + str(self.step))
-        plt.show()
         return(l)
