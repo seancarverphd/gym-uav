b635f0a319a74810912303eda7650fff52553026
Sean Carver <seancarverphd@gmail.com>
2020-09-30 15:09:44 -0400

    Fixed some bugs, runs again.

--- tele/findjam.py
+++ tele/findjam.py
@@ -7,6 +7,8 @@ class Jams():
         self.slope = slope
         self.hq = (0,0)
         self.asset = (ngrid-1,ngrid-1)
+        self.comm = None
+        self.jammer = None
         self.teleport_comm()
         self.teleport_jammer()
         self.jx = np.arange(5).reshape(1,5)*np.ones((5,1))
@@ -23,17 +25,37 @@ class Jams():
     def teleport(self, ngrid):
         return (np.random.choice(ngrid), np.random.choice(ngrid))
 
-    def logsig(self, x):
-        return np.log(scipy.special.expit(2*self.slope*x))
+    def sig(self, x):
+        return scipy.special.expit(2*self.slope*x)
 
-    def loglikelihood(self):
-        return self.logsig(np.sqrt((self.jx - self.comm[0])**2 + (self.jy - self.comm[1])**2))
+    def likelihood(self, target, jam=None):
+        if jam is None:
+            jx = self.jx  # numpy array, all locations
+            jy = self.jy  # numpy array, all locations
+        else:
+            jx = jam[0]   # single float, one location
+            jy = jam[1]   # single float, one location
+        return self.sig(np.sqrt((jx - self.comm[0])**2 + (jy - self.comm[1])**2) -
+                        np.sqrt((target[0] - self.comm[0])**2 + (target[1] - self.comm[1])**2))
+
+    def contact(self, target):
+        jam = self.jammer
+        p = self.likelihood(target, jam)
+        return np.random.choice([True, False],p=(p, 1.-p)) 
+
+    def likelihood_obs(self, target, obs):
+            self.p_success = self.likelihood(target)              # Prob at all locations
+            self.p_obs = self.p_success if obs else 1-self.p_success
+            return self.p_obs
 
     def run(self, steps=3):
         for _ in range(steps):
-            logPjammer_unnormalized = self.loglikelihood() + self.logPjammer_prior
-            self.logPjammer_prior = scipy.special.log_softmax(logPjammer_unnormalized)  # Prior updated to Posterior
-            self.render()
+            self.asset_contacted = self.contact(self.asset)       # True/False at veridical jammer location
+            self.hq_contacted = self.contact(self.hq)          # True/False at veridical jammer location
+            self.p_obs_asset = self.likelihood_obs(self.asset, self.asset_contacted)
+            self.p_obs_hq = self.likelihood_obs(self.hq, self.hq_contacted)
+            self.logPjammer_unnormalized = np.log(self.p_obs_asset) + np.log(self.p_obs_hq) + self.logPjammer_prior
+            self.logPjammer_prior = scipy.special.log_softmax(self.logPjammer_unnormalized)  # Prior updated to Posterior
             self.teleport_comm()
 
     def render(self):
