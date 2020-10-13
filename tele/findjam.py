713a85fa7187a3067b9b7c9198dfa71700868e83
Sean Carver <seancarverphd@gmail.com>
2020-10-13 14:12:52 -0400

    Working on coding the model

--- tele/findjam.py
+++ tele/findjam.py
@@ -4,30 +4,36 @@ import scipy.special
 import matplotlib.pylab as plt
 
 class Jams():
-    def __init__(self, ngrid=5, ncomms=1, njams=1, slope=10., nsteps=1, seed=None):
+    def __init__(self, ngrid=5, ncomms=1, nassets=1, njams=1, slope=10., nsteps=1, seed=None):
         self.ngrid = ngrid  # grid points on map in 1D
         self.ncomms = ncomms
+        self.nassets = nassets
+        self.nfriendly = 1 + self.ncomms + self.nassets  # 1 for headquarters
+        self.adjacency = torch.zeros((self.nfriendly, self.nfriendly), dtype=bool)
         self.njams = njams
         self.slope = slope
         self.nsteps = nsteps
         self.seed = seed
-        self.hq = (0,0)
-        self.asset = (self.ngrid-1,self.ngrid-1)
+        self.adjacency = 
+        # self.hq = (0,0)
+        # self.asset = (self.ngrid-1,self.ngrid-1)
         if self.seed is not None:
             np.random.seed(self.seed)
             torch.manual_seed(self.seed+1)
         self.comm = None
         self.jammers = None
 
+
 class JamsPoint(Jams):
     def __init__(self):
         super().__init__(**kwargs)
 
+
 class JamsGrid(Jams):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.teleport_comm()
         self.teleport_jammers()
+        self.teleport_set = {(i + 1) for i in range(self.ncomms)}
         self.step = 0  # initialize counter for number of steps
         self.Jx, self.Jy = self.makeJxy()
         self.Jx1, self.Jy1 = self.makeJxy1()
@@ -37,27 +43,62 @@ class JamsGrid(Jams):
         self.logPjammers_prior = torch.ones([self.ngrid]*2*self.njams)*(-2*self.njams)*np.log(self.ngrid) # logProb(jammers@loc); init to uniform
         self.priorshape = self.logPjammers_prior.shape
 
-    def teleport_comm(self):
+
+    def teleport_friendly(self):
         '''
-        teleport_comm: select random locations on grid for comm(s)
+        teleport_friendly: select random locations on grid for comm(s)
+                           done once during every step of the loop
         '''
         self.comm = []
-        for _ in range(self.ncomms):
+        for friend in range(self.nfriendly):
+            if friend in teleport_set: 
             self.comm.append(self.teleport(self.ngrid))
+
 
     def teleport_jammers(self):
         '''
         teleport_jammers: select random locations on grid for jammers
+                          done once on class initialization
         '''
         self.jammers = []
         for _ in range(self.njams):
             self.jammers.append(self.teleport(self.ngrid))
 
-    def teleport(self, ngrid):
+
+    def teleport_ongrid(self, ngrid):
         '''
-        teleport: select a random location on grid
+        teleport_ongrid: select a random location on grid
         '''
         return (np.random.choice(ngrid), np.random.choice(ngrid))
+
+
+    def teleport_off(self, ngrid):
+        '''
+        teleport_offgrid: select a random location within the bounds of the grid, but with probability 1, not on a gridpoint
+        '''
+        return tuple(np.random.uniform(low=0.0, high=ngrid, size=2))
+
+    
+    def friendly_move(self):
+        '''
+        friendly_move: right now a wrapper for teleport_comm but will be generalized
+        '''
+        self.teleport_comms()
+
+
+    def jammers_move(self):
+        '''
+        jammers_move: right now a stub for when jammers will move (currently they don't)
+        '''
+        pass
+
+
+    def jammers_predict(self):
+        '''
+        jammers_predict: right now does nothing because jammers don't move
+        '''
+        pass
+
 
     def itertuple(self, dims):
         '''
@@ -84,6 +125,7 @@ class JamsGrid(Jams):
             if i >= dims:
                 break
 
+
     def makeJm(self, k):
         '''
         makeJm creates an np.array of size [self.ngrid]*(2*self.njams)
@@ -96,6 +138,7 @@ class JamsGrid(Jams):
         for I in self.itertuple(2*self.njams):
             Jm[I] = I[k] 
         return Jm
+
 
     def makeJxy(self):
         '''
@@ -117,6 +160,7 @@ class JamsGrid(Jams):
         Jry = torch.tensor(np.array(Jy), dtype=float)
         return Jrx, Jry
 
+
     def makeJxy1(self):
         '''
         makeJrxy1 creates and returns 2 tensors Jrx1, and Jry1.  Each of these has shape [self.njams] -- 1D-tensor
@@ -126,16 +170,21 @@ class JamsGrid(Jams):
         Jry1 = torch.tensor([self.jammers[kj][1] for kj in range(self.njams)], dtype=float)  # each component single float, veridical y-location
         return Jrx1, Jry1
 
-    def dist_to_comm(self, jx, jy, kc=0):
+
+    def dist_to_friendly(self, jx, jy, kf=0):
         '''
         dist_to_comm computes the Euclidean distance from (cx, cy) (the comm) to (jx, jy) in the Cartesian plane
                      jx, jy could be grid tensors for jammers or 1D veridical jammer locations in x and y
                      kc^th comm
         Might generalize Euclidean distance to distance on globe, but that probably isn't necessary
         '''
-        cx = self.comm[kc][0]
-        cy = self.comm[kc][1]
+        cx = self.comm[kf][0]
+        cy = self.comm[kf][1]
         return torch.sqrt((jx - cx)**2 + (jy - cy)**2)
+
+
+    def power_of_source = 
+
 
     def distdiff(self, target, jx, jy, kc=0):
         '''
@@ -149,6 +198,7 @@ class JamsGrid(Jams):
         dist_c2t = self.dist_to_comm(targetx, targety, kc)
         return dist_c2j - dist_c2t
 
+
     def logsig(self, x):
         '''
         logsig returns the log of the sigmoid function (expit) of its argument x
@@ -159,6 +209,7 @@ class JamsGrid(Jams):
         '''
         return torch.log(scipy.special.expit(2*self.slope*x/self.ngrid))
 
+
     def loglikelihood(self, target, jx, jy, kc=0):
         '''
         loglikelihood: log-likelihood of successful communication between comm and target with specified jammer location(s)
@@ -167,17 +218,20 @@ class JamsGrid(Jams):
         ddiff = self.distdiff(target, jx, jy, kc)
         return self.logsig(ddiff).sum(axis=0)  # axis=0 is jammer num, add logs because jamming from different jammers independent
 
+
     def loglikelihood_grid(self, target, kc=0):
         '''
         loglikelihood_grid: wrapper for loglikelihood passing object's whole grid for possible jammer locations
         '''
         return self.loglikelihood(target, self.Jx, self.Jy, kc)
 
+
     def loglikelihood_veridical(self, target, kc=0):
         '''
         loglikelihood_veridical: wrapper for loglikelihood passing veridical jammer locations instead of whole grid for possible jammer locations
         '''
         return self.loglikelihood(target, self.Jx1, self.Jy1, kc)
+
 
     def loglikelihood_obs(self, target, obs):
         '''
@@ -188,12 +242,27 @@ class JamsGrid(Jams):
         log_p_success = self.loglikelihood_grid(target)
         return log_p_success if obs else torch.log(1 - torch.exp(log_p_success))
 
-    def try_to_contact(self, target):
+
+    #TODO
+    def try_to_contact(self, sender, receiver):
         '''
         try_to_contact flips a "coin" (usually unfair) to simulate success or failure to communicate based on likelihood derived from veridical jammer locations
         '''
         p = torch.exp(self.loglikelihood_veridical(target))
         return np.random.choice([True, False],p=(p, 1.-p)) 
+
+
+    def all_try(self):
+        for sender in range(nfriendly):
+            for receiver in range(nfriendly)
+                self.adjacency[sender, receiver] = self.try_to_contact(sender, receiver)
+        return(self.adjacency)
+
+
+    #TODO
+    def update_jammers(self):
+        pass
+
 
     def normalize(self, distrib):
         '''
@@ -205,11 +274,13 @@ class JamsGrid(Jams):
         flat = torch.nn.functional.log_softmax(distrib.flatten(), dim=0)  # New Prior equals normalized Posterior
         return flat.reshape(priorshape)
 
+
     def normalize_prior(self):
         '''
         normalize_prior: wrapper for normalize that normalizes prior (not used in code but might be on command line)
         '''
         return self.normalize(self.logPjammers_prior)
+
 
     def run(self, steps=None):
         '''
@@ -218,15 +289,18 @@ class JamsGrid(Jams):
         if steps is None:
             steps = self.nsteps
         for _ in range(steps):
-            # need to loop these over comms, also need to contact comms <--> comms
-            self.asset_contacted = self.try_to_contact(self.asset)       # Returns True/False using veridical jammer location(s)
-            self.hq_contacted = self.try_to_contact(self.hq)             # Returns True/False using veridical jammer location(s)
-            # The rest of the calculations should NOT use the unknown jammer location(s): instead ND-Array of all jammer locations
-            self.logPjammers_prior += self.loglikelihood_obs(self.asset, self.asset_contacted)  # Returns ND-array over locations, asset
-            self.logPjammers_prior += self.loglikelihood_obs(self.hq, self.hq_contacted)  # decomposes into sum by independence assumption
-            self.teleport_comm()
+            self.friendly_move()  # teleports all jammers to new locations
+            self.jammers_move()  # so far, doesn't do anything
+            self.jammers_predict()  # so far, doesn't do anything
+            adjacency = self.all_try()
+            self.jammers_update(adjacency)
             self.step += 1
+            # self.hq_contacted = self.try_to_contact(self.hq)             # Returns True/False using veridical jammer location(s)
+            # self.logPjammers_prior += self.loglikelihood_obs(self.hq, self.hq_contacted)  # decomposes into sum by independence assumption
+            # self.asset_contacted = self.try_to_contact(self.asset)       # Returns True/False using veridical jammer location(s)
+            # self.logPjammers_prior += self.loglikelihood_obs(self.asset, self.asset_contacted)  # Returns ND-array over locations, asset
         self.logPjammers_prior = self.normalize(self.logPjammers_prior)
+
 
     def marginal(self, joint):
         '''
@@ -242,19 +316,24 @@ class JamsGrid(Jams):
             terms_rearranged_for_sum[term_number] = joint[term_multiindex]  # each side of this assignment is a 2D tensor in x and y
         return torch.logsumexp(terms_rearranged_for_sum, dim=0)  # convert to probabilities, add across term, then retake log
 
+
     def annotations(self):
         '''
         annotations: add annotations to plot
         '''
         plt.text(self.asset[0], self.asset[1], "Asset")
-        plt.text(self.hq[0], self.hq[1], "Headquarters")
-        # plt.text(self.comm[0][0], self.comm[0][1], "Comm")
-        for kc in range(self.ncomms):
-            plt.text(self.comm[kc][0], self.comm[kc][1], "Comm")
+        for kf in range(self.friendly):
+            if kf==0:
+                plt.text(self.friendly[kf][0], self.friendly[kf][1], "Headquarters")
+            elif kf in self.teleport_set: 
+                plt.text(self.friendly[kf][0], self.friendly[kf][1], "Comm")
+            else:
+                plt.text(self.friendly[kf][0], self.friendly[kf][1], "Asset")
         for kj in range(self.njams):
             plt.text(self.jammers[kj][0], self.jammers[kj][1],"Jammer")
         plt.title("Steps = " + str(self.step))
         plt.show()
+
 
     def render(self):
         '''
@@ -262,6 +341,7 @@ class JamsGrid(Jams):
         '''
         plt.imshow(self.marginal(self.logPjammers_prior).T, cmap='hot', interpolation='nearest')  # transpose to get plot right
         self.annotations()
+
 
     def unravel_index(self, index, shape):
         '''
@@ -274,12 +354,14 @@ class JamsGrid(Jams):
             index = index // dim
         return tuple(reversed(out))
 
+
     def estimates(self):
         '''
         estimates: produces the maximum aposteriori estimates of the jammer locations based on the information currently in grid
         '''
         imax = self.logPjammers_prior.argmax()
         return self.unravel_index(imax, tuple([self.ngrid]*(2*self.njams)))
+
 
     def logjoint_iid_from_logmarginal(self, logmarginal):
         '''
@@ -293,6 +375,7 @@ class JamsGrid(Jams):
                 logjoint_iid[index_joint] += logmarginal[index_joint[2*kj], index_joint[2*kj+1]]
         return logjoint_iid
 
+
     def conditional(self, joint, freeze):
         '''
         conditional: create the conditional distribution from the joint distribution
@@ -301,16 +384,19 @@ class JamsGrid(Jams):
         '''
         return self.normalize(joint[freeze])
 
+
     def show_conditional(self, freeze):
         assert len(freeze) + 2 == len(self.logPjammers_prior.shape)
         plt.imshow(self.conditional(self.logPjammers_prior, freeze).T, cmap='hot', interpolation='nearest')  # transpose to get plot right
         self.annotations()
+
 
     def test_independence(self):
         logjoint = self.logPjammers_prior
         logmargin = self.marginal(logjoint)
         logjoint_iid = self.logjoint_iid_from_logmarginal(logmargin)
         return logjoint_iid.allclose(logjoint)
+
 
 class test1D():
     '''
@@ -320,17 +406,21 @@ class test1D():
         self.ngrid = ngrid
         self.slope = 1
 
+
     def run(self):
         self.grid = torch.zeros((self.ngrid, self.ngrid))
         for j in range(self.ngrid):
             for c in range(self.ngrid):
                 self.grid[j, c] = torch.abs(torch.tensor(c) - torch.tensor(j)) - torch.abs(torch.tensor(c))
 
+
     def render0(self):
         l = plt.imshow(self.grid, cmap='hot', interpolation='nearest')
 
+
     def logsig(self, x):
         return torch.log(scipy.special.expit(torch.true_divide(2*self.slope*x, self.ngrid)))
+
 
     def render2(self):
         l = plt.imshow(self.logsig(self.grid), cmap='hot', interpolation='nearest')
