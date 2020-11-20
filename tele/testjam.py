12e2256d93e5ca6145379ddcd88630bc75f3e666
Sean Carver <seancarverphd@gmail.com>
2020-11-19 21:51:03 -0500

    Added another test to verify shape of loglikelihood_grid plus a setup function in test

--- tele/testjam.py
+++ tele/testjam.py
@@ -1,6 +1,14 @@
 import findjam
 
-def test_one_jam():
+def setup_module():
+    global J
     J = findjam.JamsGrid(ngrid=8, ncomms=1, njams=1, move=True, seed=0)
     J.run(10)
+
+def test_one_jam():
+    global J
     assert J.estimates() == (6, 5)
+
+def test_likelihood_shape():
+    global J
+    assert J.loglikelihood_grid().shape == (3,3,8,8)  # 3 friendlys and 8x8 grid
