426596c9b044b57f2c784e4c8c28041b6b222d3c
Sean Carver <seancarverphd@gmail.com>
2020-11-25 15:16:01 -0500

    Added tests for 2 jammers

--- tele/testjam.py
+++ tele/testjam.py
@@ -1,14 +1,29 @@
 import findjam
 
 def setup_module():
-    global J
-    J = findjam.JamsGrid(ngrid=8, ncomms=1, njams=1, move=True, seed=0)
-    J.run(10)
+    global J1, J2
+    J1 = findjam.JamsGrid(ngrid=8, ncomms=1, njams=1, move=True, seed=0)
+    J1.run(10)
+    J2 = findjam.JamsGrid(ngrid=6, ncomms=1, njams=2, move=True, seed=0)
+    J2.run(1)
 
-def test_one_jam():
-    global J
-    assert J.estimates() == (6, 5)
 
-def test_likelihood_shape():
-    global J
-    assert J.loglikelihood_grid().shape == (3,3,8,8)  # 3 friendlys and 8x8 grid
+def test_estimate_jam1():
+    global J1
+    assert J1.estimates() == (6, 5)
+
+
+def test_estimate_jam2():
+    global J2
+    assert J2.estimates() == (1, 4, 1, 1)
+
+
+def test_likelihood_shape1():
+    global J1
+    assert J1.loglikelihood_grid().shape == (3,3,8,8)  # 3 friendlys and 8x8 grid
+
+
+def test_likelihood_shape2():
+    global J2
+    assert J2.loglikelihood_grid().shape == (3,3,6,6,6,6)  # 3 friendlys and 8x8 grid
+
