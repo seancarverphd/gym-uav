6cc6543e41112ee030392f2e11bd013df08248e7
Sean Carver <seancarverphd@gmail.com>
2021-01-04 16:53:02 -0500

    Working on classes of game module

--- minigame/game.py
+++ minigame/game.py
@@ -4,21 +4,33 @@ import torch
 class Faction():
     def __init__(self, name):
         self.name = name
+        self.units = []
+
+    def add_unit(self, unit):
+        self.units.append(unit)
+        self.units[-1].faction = self
+
+    def clear_units(self):
+        for u in self.units:
+            u.faction = None
+        self.units = []
+
 
 class Unit():
-    def __init__(self):
+    def __init__(self, init_x, init_y):
         self.faction = None
         self.receiver = None
         self.transmitter = None
         self.name = 'GHOST'
         self.communicates = False
         self.jams = False
+        self.shoots = False
         self.flies = False
         self.traverses_roads = False
-        self.occupying_building = False
-        self.shoots_drones = False
-        self.x_ = 0.
-        self.y_ = 0.
+        self.occupies_roofs = False
+        self.on_roof = False
+        self.x_ = init_x
+        self.y_ = init_y
 
     def x(self):
         return x_
@@ -33,5 +45,9 @@ class Unit():
         pass
 
 
+class Order():
+    def __init__(self):
+        destination = None
+        occupy_roof = False
+        shoot_enemy_drones = False
 
-         
