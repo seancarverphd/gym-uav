39b304d03b16dc921101c3299a81e3c7cf0fdcd4
Sean Carver <seancarverphd@gmail.com>
2021-01-10 11:00:59 -0500

    Reorganized tests

--- minigame/testgame.py
+++ minigame/testgame.py
@@ -3,26 +3,29 @@ import numpy as np
 import torch
 
 
-# def setup_module():
-
-def test_that_unit_superclasses_load():
-    GHO = game.Unit()                                                                                                                                                                                   
-    DRO = game.Drone()                                                                                                                                                                                  
-    GRO = game.GroundTroop()                                                                                                                                                                            
-    # Test fails if any of the above raise an error
-
-def test_that_factions_build():
+def setup_module():
+    global BLUE, RED
     BLUE = game.Faction('BLUE')
-    COM = game.Comm()                                                                                                                                                                                   
+    COM = game.Comm()
     OCC = game.OccupyingTroop()                                                                                                                                                                         
     BLUE.add_unit(COM)
     BLUE.add_unit(OCC)
     RED = game.Faction('RED')
-    JAM = game.Jammer()                                                                                                                                                                                 
+    JAM = game.Jammer()
     ROA = game.RoamingTroop()                                                                                                                                                                           
     RED.add_unit(JAM)
     RED.add_unit(ROA)
+
+
+def assert_correct_names_for_units():
     assert BLUE.units[0].name == 'COMM'
     assert BLUE.units[1].name == 'OCCUPYING_TROOP'
     assert RED.units[0].name == 'JAMMER'
     assert RED.units[1].name == 'ROAMING_TROOP'
+
+
+def test_that_unit_superclasses_load():
+    GHO = game.Unit()
+    DRO = game.Drone()
+    GRO = game.GroundTroop()
+
