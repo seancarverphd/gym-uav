17aa2e6e42db31450fc7c22468c7e5a2c47f3c27
Sean Carver <seancarverphd@gmail.com>
2021-01-10 10:23:36 -0500

    Loads Faction; test added

--- minigame/testgame.py
+++ minigame/testgame.py
@@ -5,12 +5,24 @@ import torch
 
 # def setup_module():
 
-def test_that_unit_classes_load():
+def test_that_unit_superclasses_load():
     GHO = game.Unit()                                                                                                                                                                                   
     DRO = game.Drone()                                                                                                                                                                                  
-    COM = game.Comm()                                                                                                                                                                                   
-    JAM = game.Jammer()                                                                                                                                                                                 
     GRO = game.GroundTroop()                                                                                                                                                                            
-    OCC = game.OccupyingTroop()                                                                                                                                                                         
-    ROA = game.RoamingTroop()                                                                                                                                                                           
     # Test fails if any of the above raise an error
+
+def test_that_factions_build():
+    BLUE = game.Faction('BLUE')
+    COM = game.Comm()                                                                                                                                                                                   
+    OCC = game.OccupyingTroop()                                                                                                                                                                         
+    BLUE.add_unit(COM)
+    BLUE.add_unit(OCC)
+    RED = game.Faction('RED')
+    JAM = game.Jammer()                                                                                                                                                                                 
+    ROA = game.RoamingTroop()                                                                                                                                                                           
+    RED.add_unit(JAM)
+    RED.add_unit(ROA)
+    assert BLUE.units[0].name == 'COMM'
+    assert BLUE.units[1].name == 'OCCUPYING_TROOP'
+    assert RED.units[0].name == 'JAMMER'
+    assert RED.units[1].name == 'ROAMING_TROOP'
