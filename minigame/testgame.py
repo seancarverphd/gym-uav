de5f8b483e0ad4c61716a080938a60264ece9b2e
Sean Carver <seancarverphd@gmail.com>
2021-01-11 19:02:19 -0500

    Now using classes for nosetests

--- minigame/testgame.py
+++ minigame/testgame.py
@@ -2,54 +2,57 @@ import game
 import numpy as np
 import torch
 
+class TestTheGame():
 
-def setup_module():
-    global BLUE, RED, GAME
-    GAME = game.GAME1
-    BLUE = game.Faction('BLUE', GAME)
-    COM = game.Comm()
-    OCC = game.OccupyingTroop()
-    BLUE.add_unit(COM)
-    BLUE.add_unit(OCC)
-    RED = game.Faction('RED', GAME)
-    JAM = game.Jammer()
-    ROA = game.RoamingTroop()
-    RED.add_unit(JAM)
-    RED.add_unit(ROA)
-    COM.order.set_destination(0.9, 0.9)
-    JAM.order.set_destination(0.1, 0.9)
-    ROA.order.set_destination(0.9, 0.1)
+    def setup(self):
+        self.GAME = game.GAME1
+        self.BLUE = game.Faction('BLUE', self.GAME)
+        self.GAME.add_blue(self.BLUE)
+        self.COM = game.Comm()
+        self.OCC = game.OccupyingTroop()
+        self.BLUE.add_unit(self.COM)
+        self.BLUE.add_unit(self.OCC)
+        self.RED = game.Faction('RED', self.GAME)
+        self.GAME.add_red(self.RED)
+        self.JAM = game.Jammer()
+        self.ROA = game.RoamingTroop()
+        self.RED.add_unit(self.JAM)
+        self.RED.add_unit(self.ROA)
+        self.COM.order.set_destination(0.9, 0.9)
+        self.JAM.order.set_destination(0.1, 0.9)
+        self.ROA.order.set_destination(0.9, 0.1)
 
-def test_fly_submax_requested():
-    U = game.Comm()
-    U.regame(GAME1)
-    assert U.x_ == .1
-    assert U.y_ == .1
-    U.order.set_destination(.2, .15)
-    U.initialize()
-    U.move()
-    assert U.x_ == .2
-    assert U.y_ == .15
+    def test_fly_submax_requested(self):
+        assert False
+        U = game.Comm()
+        self.U.regame(GAME1)
+        assert U.x_ == .1
+        assert U.y_ == .1
+        U.order.set_destination(.2, .15)
+        U.initialize()
+        U.move()
+        assert U.x_ == .2
+        assert U.y_ == .15
 
-def test_fly_submax_requested():
-    U = game.Comm()
-    assert U.x_ == .1
-    assert U.y_ == .1
-    #TODO Finish this test
+    def test_fly_submax_requested(self):
+        U = game.Comm()
+        assert U.x_ == .1
+        assert U.y_ == .1
+        #TODO Finish this test
 
-def test_correct_names_for_units():
-    assert BLUE.units[0].name == 'COMM'
-    assert BLUE.units[1].name == 'OCCUPYING_TROOP'
-    assert RED.units[0].name == 'JAMMER'
-    assert RED.units[1].name == 'ROAMING_TROOP'
+    def test_correct_names_for_units(self):
+        assert self.BLUE.units[0].name == 'COMM'
+        assert self.BLUE.units[1].name == 'OCCUPYING_TROOP'
+        assert self.RED.units[0].name == 'JAMMER'
+        assert self.RED.units[1].name == 'ROAMING_TROOP'
 
-def test_commands():
-    BLUE.initialize()
-    RED.initialize()
-    BLUE.implement_ceoi()
-    RED.implement_ceoi()
-    BLUE.move()
-    RED.move()
-    BLUE.post_timestep()
-    RED.post_timestep()
+    def test_commands(self):
+        self.BLUE.initialize()
+        self.RED.initialize()
+        self.BLUE.implement_ceoi()
+        self.RED.implement_ceoi()
+        self.BLUE.move()
+        self.RED.move()
+        self.BLUE.post_timestep()
+        self.RED.post_timestep()
 
