f24323dc63d9bc1a2dcf24030d2e1873826c93c9
Sean Carver <seancarverphd@gmail.com>
2021-02-19 11:37:44 -0500

    test_net added, not yet interfaced with env

--- train.py
+++ train.py
@@ -4,7 +4,6 @@ import time
 import math
 import ptan
 import gym
-import pybullet_envs
 import argparse
 from tensorboardX import SummaryWriter
 
@@ -16,7 +15,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
-ENV_ID = "MinitaurBulletEnv-v0"
+ENV_ID = "uav-v0"
 GAMMA = 0.99
 REWARD_STEPS = 2
 BATCH_SIZE = 32
@@ -26,7 +25,7 @@ ENTROPY_BETA = 1e-4
 TEST_ITERS = 1000
 
 
-def test_net(net, env, count=10, device="cpu"):
+def test_net(net, env, count=40, device="cpu"):
     rewards = 0.0
     steps = 0
     for _ in range(count):
@@ -34,10 +33,12 @@ def test_net(net, env, count=10, device="cpu"):
         while True:
             obs_v = ptan.agent.float32_preprocessor([obs])
             obs_v = obs_v.to(device)
-            mu_v = net(obs_v)[0]
-            action = mu_v.squeeze(dim=0).data.cpu().numpy()
-            action = np.clip(action, -1, 1)
-            obs, reward, done, _ = env.step(action)
+            net_obs_v = net(obs_v)
+            mean_x_v = net_obs_v[0]
+            mean_x_v = net_obs_v[1]
+            action = numpy.array([mean_x_v.squeeze(dim=0).data.cpu().numpy().clip(0, 7), \ # first arg of clip was action TODO Generalize beyond 8x8
+                    mean_y_v.squeeze(dim=0).data.cpu().numpy().clip(0, 7)])  # TODO Check dims
+            obs, reward, done, _ = env.step(action)  #TODO Check OK
             rewards += reward
             steps += 1
             if done:
