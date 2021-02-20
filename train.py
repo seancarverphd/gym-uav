dfe3eb8245a886dea6152cc6f0b122518e45410b
Sean Carver <seancarverphd@gmail.com>
2021-02-20 04:43:31 -0500

    Added calc_logprob function and adapted for bivariate normal

--- train.py
+++ train.py
@@ -58,3 +58,95 @@ def test_net(net, env, count=40, device="cpu"):
                 break
     return rewards / count, steps / count
 
+def calc_logprob(mu_v, cov_v, actions_v):
+    U, D, V = torch.svd(cov_v)
+    p1 = - 1./2.*(actions_v-mu_v).T@V@torch.diagflat(1./D[0].clamp(min=1e-3),1./D[1].clamp(min=1e-3))@U.T@(actions_v-mu_v)
+    p2 = - torch.log(2 * math.pi)
+    p3 = - 1./2. torch.logdet(U@torch.diagflat(D[0].clamp(min=1e-3), D[1].clamp(min=1e3))@V.T)
+    return p1 + p2 + p3
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action='store_true', help='Enable CUDA')
+    parser.add_argument("-n", "--name", required=True, help="Name of the run")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    save_path = os.path.join("saves", "a2c-" + args.name)
+    os.makedirs(save_path, exist_ok=True)
+
+    env = gym.make(ENV_ID)
+    test_env = gym.make(ENV_ID)
+
+    net = model.ModelA2C(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
+    print(net)
+
+    writer = SummaryWriter(comment="-a2c_" + args.name)
+    agent = model.AgentA2C(net, device=device)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, GAMMA, steps_count=REWARD_STEPS)
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    batch = []
+    best_reward = None
+    with ptan.common.utils.RewardTracker(writer) as tracker:
+        with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
+            for step_idx, exp in enumerate(exp_source):
+                rewards_steps = exp_source.pop_rewards_steps()
+                if rewards_steps:
+                    rewards, steps = zip(*rewards_steps)
+                    tb_tracker.track("episode_steps", steps[0], step_idx)
+                    tracker.reward(rewards[0], step_idx)
+
+                if step_idx % TEST_ITERS == 0:
+                    ts = time.time()
+                    rewards, steps = test_net(net, test_env, device=device)
+                    print("Test done is %.2f sec, reward %.3f, steps %d" % (
+                        time.time() - ts, rewards, steps))
+                    writer.add_scalar("test_reward", rewards, step_idx)
+                    writer.add_scalar("test_steps", steps, step_idx)
+                    if best_reward is None or best_reward < rewards:
+                        if best_reward is not None:
+                            print("Best reward updated: %.3f -> %.3f" % (best_reward, rewards))
+                            name = "best_%+.3f_%d.dat" % (rewards, step_idx)
+                            fname = os.path.join(save_path, name)
+                            torch.save(net.state_dict(), fname)
+                        best_reward = rewards
+
+                batch.append(exp)
+                if len(batch) < BATCH_SIZE:
+                    continue
+
+                states_v, actions_v, vals_ref_v = \
+                    common.unpack_batch_a2c(
+                        batch, net, device=device,
+                        last_val_gamma=GAMMA ** REWARD_STEPS)
+                batch.clear()
+
+                optimizer.zero_grad()
+                mu_v, var_v, value_v = net(states_v)
+
+                loss_value_v = F.mse_loss(
+                    value_v.squeeze(-1), vals_ref_v)
+
+                adv_v = vals_ref_v.unsqueeze(dim=-1) - \
+                        value_v.detach()
+                log_prob_v = adv_v * calc_logprob(
+                    mu_v, var_v, actions_v)
+                loss_policy_v = -log_prob_v.mean()
+                ent_v = -(torch.log(2*math.pi*var_v) + 1)/2
+                entropy_loss_v = ENTROPY_BETA * ent_v.mean()
+
+                loss_v = loss_policy_v + entropy_loss_v + \
+                         loss_value_v
+                loss_v.backward()
+                optimizer.step()
+
+                tb_tracker.track("advantage", adv_v, step_idx)
+                tb_tracker.track("values", value_v, step_idx)
+                tb_tracker.track("batch_rewards", vals_ref_v, step_idx)
+                tb_tracker.track("loss_entropy", entropy_loss_v, step_idx)
+                tb_tracker.track("loss_policy", loss_policy_v, step_idx)
+                tb_tracker.track("loss_value", loss_value_v, step_idx)
+                tb_tracker.track("loss_total", loss_v, step_idx)
+
